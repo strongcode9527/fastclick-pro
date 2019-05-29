242d5384900e0a9357c5f78f33d1ab236523521b
QITMAC000948 <1477869317@qq.com>
2019-05-29 16:00:33 +0800

    :sparkles:

---
+++ utils/index.js
@@ -0,0 +1,50 @@
+
+export function isTypeElement(target) {
+  const targetTagName = (target.tagName || '').toLowerCase()
+  return targetTagName === 'textarea' || (targetTagName === 'input' && target.type === 'text');
+}
+
+/**
+ * Windows Phone 8.1 fakes user agent string to look like Android and iPhone.
+ *
+ * @type boolean
+ */
+export const deviceIsWindowsPhone = navigator.userAgent.indexOf("Windows Phone") >= 0;
+
+/**
+ * Android requires exceptions.
+ *
+ * @type boolean
+ */
+export const deviceIsAndroid = navigator.userAgent.indexOf('Android') > 0 && !deviceIsWindowsPhone;
+
+
+/**
+ * iOS requires exceptions.
+ *
+ * @type boolean
+ */
+export const deviceIsIOS = /iP(ad|hone|od)/.test(navigator.userAgent) && !deviceIsWindowsPhone;
+
+
+/**
+ * iOS 4 requires an exception for select elements.
+ *
+ * @type boolean
+ */
+export const deviceIsIOS4 = deviceIsIOS && (/OS 4_\d(_\d)?/).test(navigator.userAgent);
+
+
+/**
+ * iOS 6.0-7.* requires the target element to be manually derived
+ *
+ * @type boolean
+ */
+export const deviceIsIOSWithBadTarget = deviceIsIOS && (/OS [6-7]_\d/).test(navigator.userAgent);
+
+/**
+ * BlackBerry requires exceptions.
+ *
+ * @type boolean
+ */
+export const deviceIsBlackBerry10 = navigator.userAgent.indexOf('BB10') > 0;
